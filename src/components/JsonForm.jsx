612c9e3f93ebb0e874ff5f203b19a0ed273bded3
Akshit <91170781+HYakshit@users.noreply.github.com>
2025-11-15 20:32:58 +0530

    feat : form styling and node trigger type added

--- src/components/JsonForm.jsx
+++ src/components/JsonForm.jsx
@@ -1,17 +1,10 @@
 // JsonForm.jsx
-import React from "react";
 import Form from "@rjsf/core";
 import validator from "@rjsf/validator-ajv8";
+import TriggerUiSchema from "../formData/Trigger/TriggerUiSchema";
+import TriggerSchema from "../formData/Trigger/TriggerSchema";
 
-const schema = {
-  title: "Todo",
-  type: "object",
-  required: ["title"],
-  properties: {
-    title: { type: "string", title: "Title", default: "A new task" },
-    done: { type: "boolean", title: "Done?", default: false },
-  },
-};
+
 
 const JsonForm = () => {
   const log = (type) => console.log.bind(console, type);
@@ -19,11 +12,12 @@ const JsonForm = () => {
   return (
     <div style={{ maxWidth: 500, margin: "0 auto" }}>
       <Form
-        schema={schema}
+        schema={TriggerSchema}
+        uiSchema={TriggerUiSchema}
         validator={validator}
-        onChange={log("changed")}
-        onSubmit={log("submitted")}
-        onError={log("errors")}
+        // onChange={log("changed")}
+        // onSubmit={log("submitted")}
+        // onError={log("errors")}
       />
     </div>
   );
--- src/components/PropertiesPanel.jsx
+++ src/components/PropertiesPanel.jsx
@@ -36,140 +36,15 @@ export default function PropertiesPanel({ selectedNode, onUpdateNode }) {
           <h2 className="text-lg font-semibold">{selectedNode.label}</h2>
           <p className="text-xs text-gray-500">{selectedNode.type}</p>
         </div>
-
         <div className="  overflow-y-auto h-[calc(100%-72px)] flex flex-col">
-          {/* ---------- GENERAL INFO SECTION ---------- */}
-          <Section
-            title="General information"
-            open={openSections.general}
-            toggle={() => toggleSection("general")}
-          >
-            <div className="flex p-3 flex-col gap-4">
-              {/* Trigger Type */}
-              <label className="text-sm font-medium text-gray-600">
-                Trigger Type
-              </label>
-              <select
-                className="input"
-                value={selectedNode.triggerType || ""}
-                onChange={(e) => onUpdateNode({ triggerType: e.target.value })}
-              >
-                <option>Time-based Trigger</option>
-                <option>Manual Trigger</option>
-              </select>
-
-              {/* Title */}
-              <div>
-                <label className="text-sm font-medium text-gray-600">
-                  Title
-                </label>
-                <input
-                  className="input"
-                  value={selectedNode.title || ""}
-                  onChange={(e) => onUpdateNode({ title: e.target.value })}
-                />
-              </div>
-
-              {/* Status */}
-              <div>
-                <label className="text-sm font-medium text-gray-600">
-                  Status
-                </label>
-                <select
-                  className="input"
-                  value={selectedNode.status}
-                  onChange={(e) => onUpdateNode({ status: e.target.value })}
-                >
-                  <option value="active">Active</option>
-                  <option value="inactive">Inactive</option>
-                </select>
-              </div>
-
-              {/* Description */}
-              <div>
-                <label className="text-sm font-medium text-gray-600">
-                  Description
-                </label>
-                <textarea
-                  className="input min-h-[70px]"
-                  value={selectedNode.description || ""}
-                  onChange={(e) =>
-                    onUpdateNode({ description: e.target.value })
-                  }
-                />
-              </div>
-            </div>
-          </Section>
-
-          {/* ---------- TRIGGER SCHEDULE SECTION ---------- */}
-          {selectedNode.type === "trigger" && (
-            <Section
-              title="Trigger Schedule"
-              open={openSections.schedule}
-              toggle={() => toggleSection("schedule")}
-            >
-              <div className="flex p-3 flex-col gap-4">
-                <label className="flex items-center gap-2">
-                  <input type="checkbox" />
-                  All Day
-                </label>
-
-                <div className="flex gap-2">
-                  <div>
-                    <label className="text-sm font-medium text-gray-600">
-                      Starts
-                    </label>
-                    <input type="date" className="input" />
-                  </div>
-                  <div>
-                    <label className="text-sm font-medium text-gray-600">
-                      Time
-                    </label>
-                    <input type="time" className="input" />
-                  </div>
-                </div>
-
-                <div className="flex gap-2">
-                  <div>
-                    <label className="text-sm font-medium text-gray-600">
-                      Ends
-                    </label>
-                    <input type="date" className="input" />
-                  </div>
-                  <div>
-                    <label className="text-sm font-medium text-gray-600">
-                      Time
-                    </label>
-                    <input type="time" className="input" />
-                  </div>
-                </div>
-
-                <div>
-                  <label className="text-sm font-medium text-gray-600">
-                    Frequency
-                  </label>
-                  <select className="input">
-                    <option>None</option>
-                    <option>Daily</option>
-                    <option>Weekly</option>
-                    <option>Monthly</option>
-                  </select>
-                </div>
-
-                <div>
-                  <label className="text-sm font-medium text-gray-600">
-                    Time zone
-                  </label>
-                  <select className="input">
-                    <option>UTC +5:30</option>
-                    <option>UTC -5</option>
-                  </select>
-                </div>
-              </div>
-            </Section>
-          )}
-
           {/* ---------- Form SECTION ---------- */}
+          <div>
+            {/* sub HEADER */}
+            <div className="px-5 py-4 border-b">
+              <h2 className="text-lg font-semibold">{selectedNode.label}</h2>
+              <p className="text-xs text-gray-500">{selectedNode.type}</p>
+            </div>
+          </div>
           <Section
             title="Form information"
             open={openSections.Form}
